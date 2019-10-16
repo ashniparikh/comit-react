144b6447fa8bf451b628e44571b36a830567d3f1
ashniparikh <ashniparikh94@gmail.com>
2019-10-15 19:45:04 -0600

    Completed Lesson 19

---
+++ lessons/19-js-variables/index.js
@@ -0,0 +1,19 @@
+const monthOfBirth = "June";
+const firstName = "Ashni";
+const lastName = "Parikh";
+
+let city;
+let age;
+let nextCity;
+
+city = "Vancouver";
+age = 24;
+nextCity = city;
+
+console.log("Month of Birth:", monthOfBirth);
+console.log("First name:", firstName);
+console.log("Last Name:", lastName);
+console.log("City:", city);
+console.log("Age:", age);
+console.log("Next City:", nextCity);
+console.log("Full Name:", `${firstName} ${lastName}`);
