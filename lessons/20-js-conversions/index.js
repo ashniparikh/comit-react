3d2e6b5b2496d58260fa95b13fd348f6b92a5808
ashniparikh <ashniparikh94@gmail.com>
2019-10-15 20:23:18 -0600

    Completed lesson 20

---
+++ lessons/20-js-conversions/index.js
@@ -0,0 +1,27 @@
+let age;
+const firstName = "Ashni";
+let courseCompletion;
+let finalScore;
+
+let winningNumbers;
+age = 24;
+courseCompletion = Boolean(1);
+finalScore = null;
+
+console.log("Age:", age);
+console.log("First Name:", firstName);
+console.log("Course Completion:", courseCompletion);
+console.log("Final Score:", finalScore);
+console.log("Winning Numbers:", winningNumbers);
+
+
+
+courseCompletion = String(1);
+finalScore = Boolean("");
+age = Number(25);
+
+console.log("Age:", age);
+console.log("First Name:", firstName);
+console.log("Course Completion:", courseCompletion);
+console.log("Final Score:", finalScore);
+console.log("Winning Numbers:", winningNumbers);
